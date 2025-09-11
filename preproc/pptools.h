640cd555fa3b601b3bcc75b501b668c653949bef
Stepainpy <stepa2014vlg@gmail.com>
2025-09-11 17:50:14 +0300

    add deg2rad and rad2deg

--- preproc/pptools.h
+++ preproc/pptools.h
@@ -1,7 +1,9 @@
 #ifndef PREPROCESSOR_TOOLS_H
 #define PREPROCESSOR_TOOLS_H
 
+#define _USE_MATH_DEFINES
 #include <limits.h>
+#include <math.h>
 
 #define PPT_PPCAT_F(x, y) x ## y
 #define PPT_PPCAT(x, y) PPT_PPCAT_F(x, y)
@@ -14,12 +16,17 @@
 
 #define ppt_mod(x, y) (((x) % (y) + (y)) % (y))
 
+#define ppt_deg_to_rad(deg) ((deg) / 180. * M_PI)
+#define ppt_rad_to_deg(rad) ((rad) / M_PI * 180.)
+
 #ifdef PPTOOLS_STRIP_PREFIX
 #  define PPCAT PPT_PPCAT
 #  define PPSTR PPT_PPSTR
 #  define countof ppt_countof
 #  define bitsof ppt_bitsof
 #  define mod ppt_mod
+#  define deg_to_rad ppt_deg_to_rad
+#  define rad_to_deg ppt_rad_to_deg
 #endif /* PPTOOLS_STRIP_PREFIX */
 
 #endif /* PREPROCESSOR_TOOLS_H */
