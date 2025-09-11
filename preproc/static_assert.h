cd11c56651ada00af348730d9bdb04319ae5294b
Stepainpy <stepa2014vlg@gmail.com>
2025-09-11 19:47:56 +0300

    update static assert

--- preproc/static_assert.h
+++ preproc/static_assert.h
@@ -1,12 +1,16 @@
 #ifndef STATIC_ASSERT_H
 #define STATIC_ASSERT_H
 
-#define CONCAT_(x, y) x ## y
-#define CONCAT(x, y) CONCAT_(x, y)
-
 #ifndef static_assert
-#define static_assert(cond, msg) \
-typedef char CONCAT(static_assert_, __LINE__)[!!(cond) * 2 - 1]
+#  if __STDC_VERSION__ >= 201112L
+#    define static_assert _Static_assert
+#  else
+#    define SA_PPCAT_F(x, y) x ## y
+#    define SA_PPCAT(x, y) SA_PPCAT_F(x, y)
+#    define static_assert(expression, message) \
+typedef char SA_PPCAT(static_assert_array_, __LINE__) \
+[(expression) && (message) ? 1 : -1]
+#  endif
 #endif
 
 #endif /* STATIC_ASSERT_H */
