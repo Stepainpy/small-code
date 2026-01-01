86d7afbfedf54638b80cc8975c9c647eb1641fba
Stepainpy <stepa2014vlg@gmail.com>
2026-01-01 18:51:51 +0300

    remove gf28_add

--- gf28.h
+++ gf28.h
@@ -1,10 +1,10 @@
 /* Irreducible polynomials (hex/dec) and first primitive element:
- * | 11b  283  3 | 11d  285  2 | 12b  299  2 | 12d  301  2 | 139  313  3 |
- * | 13f  319  3 | 14d  333  2 | 15f  351  2 | 163  355  2 | 165  357  2 |
- * | 169  361  2 | 171  369  2 | 177  375  3 | 17b  379  9 | 187  391  2 |
- * | 18b  395  6 | 18d  397  2 | 19f  415  3 | 1a3  419  3 | 1a9  425  2 |
- * | 1b1  433  6 | 1bd  445  7 | 1c3  451  2 | 1cf  463  2 | 1d7  471  7 |
- * | 1dd  477  6 | 1e7  487  2 | 1f3  499  6 | 1f5  501  2 | 1f9  505  3 |
+ * 11b  283  3 | 11d  285  2 | 12b  299  2 | 12d  301  2 | 139  313  3
+ * 13f  319  3 | 14d  333  2 | 15f  351  2 | 163  355  2 | 165  357  2
+ * 169  361  2 | 171  369  2 | 177  375  3 | 17b  379  9 | 187  391  2
+ * 18b  395  6 | 18d  397  2 | 19f  415  3 | 1a3  419  3 | 1a9  425  2
+ * 1b1  433  6 | 1bd  445  7 | 1c3  451  2 | 1cf  463  2 | 1d7  471  7
+ * 1dd  477  6 | 1e7  487  2 | 1f3  499  6 | 1f5  501  2 | 1f9  505  3
  */
 
 #ifndef GALOIS_FIELD_2P8_H
@@ -16,7 +16,6 @@
 #define GF28_DFLT_IP 0x1b
 #endif
 
-#define gf28_add(lhs, rhs) ((uint8_t)(lhs) ^ (uint8_t)(rhs))
 #define gf28_mul(lhs, rhs) gf28_mul_ip((lhs), (rhs), GF28_DFLT_IP)
 #define gf28_inv(mhs     ) gf28_inv_ip((mhs),        GF28_DFLT_IP)
 
