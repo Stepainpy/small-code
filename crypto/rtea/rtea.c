62bc71410a24d44cef5294100646b8859158be8a
Stepainpy <stepa2014vlg@gmail.com>
2026-01-26 21:22:17 +0300

    update rtea cipher

---
+++ crypto/rtea/rtea.c
@@ -0,0 +1,358 @@
+#include "rtea.h"
+#include <string.h>
+
+#define RTEAI_BLKSZ 8
+#define RTEAI_BLKFILLER 0x01
+
+#define rteai_xor(dst, lhs, rhs) do { \
+    dst[0] = lhs[0] ^ rhs[0]; \
+    dst[1] = lhs[1] ^ rhs[1]; \
+} while (0)
+
+#define rteai_xora(dst, rhs) do { \
+    dst[0] ^= rhs[0]; \
+    dst[1] ^= rhs[1]; \
+} while (0)
+
+#define rteai_inc(blk) do { \
+    if (!++blk[0]) ++blk[1]; \
+} while (0)
+
+typedef void (*rteai_onepass_t)(rtea_block_t, const rtea_block_t, const rtea_word_t*);
+
+static int rteai_en_ecb(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out; size_t got;
+    do {
+        memset(in, RTEAI_BLKFILLER, RTEAI_BLKSZ);
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+
+        onepass(out, in, param.key);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    } while (got == RTEAI_BLKSZ);
+    return RTEAI_BLKSZ - got;
+}
+
+static int rteai_en_cbc(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out, prev; size_t got;
+    memcpy(prev, param.init_vec, RTEAI_BLKSZ);
+    do {
+        memset(in, RTEAI_BLKFILLER, RTEAI_BLKSZ);
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+
+        rteai_xora(in, prev);
+        onepass(out, in, param.key);
+        memcpy(prev, out, RTEAI_BLKSZ);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    } while (got == RTEAI_BLKSZ);
+    return RTEAI_BLKSZ - got;
+}
+
+static int rteai_en_cfb(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out, prev; size_t got;
+    memcpy(prev, param.init_vec, RTEAI_BLKSZ);
+    do {
+        memset(in, RTEAI_BLKFILLER, RTEAI_BLKSZ);
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+
+        onepass(out, prev, param.key);
+        rteai_xora(out, in);
+        memcpy(prev, out, RTEAI_BLKSZ);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    } while (got == RTEAI_BLKSZ);
+    return RTEAI_BLKSZ - got;
+}
+
+static int rteai_en_ofb(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out, prev; size_t got;
+    memcpy(prev, param.init_vec, RTEAI_BLKSZ);
+    do {
+        memset(in, RTEAI_BLKFILLER, RTEAI_BLKSZ);
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+
+        onepass(out, prev, param.key);
+        memcpy(prev, out, RTEAI_BLKSZ);
+        rteai_xora(out, in);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    } while (got == RTEAI_BLKSZ);
+    return RTEAI_BLKSZ - got;
+}
+
+static int rteai_en_ctr(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out, ctr; size_t got;
+    memcpy(ctr, param.init_vec, RTEAI_BLKSZ);
+    do {
+        memset(in, RTEAI_BLKFILLER, RTEAI_BLKSZ);
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+
+        onepass(out, ctr, param.key);
+        rteai_xora(out, in);
+        rteai_inc(ctr);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    } while (got == RTEAI_BLKSZ);
+    return RTEAI_BLKSZ - got;
+}
+
+static int rteai_en_pcbc(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out, prev; size_t got;
+    memcpy(prev, param.init_vec, RTEAI_BLKSZ);
+    do {
+        memset(in, RTEAI_BLKFILLER, RTEAI_BLKSZ);
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+
+        rteai_xora(prev, in);
+        onepass(out, prev, param.key);
+        rteai_xor(prev, in, out);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    } while (got == RTEAI_BLKSZ);
+    return RTEAI_BLKSZ - got;
+}
+
+static int rteai_en(
+    const rtea_io_t* io, rtea_param_t param,
+    rtea_mode_t mode, rteai_onepass_t enfn, rteai_onepass_t defn
+) { (void)defn;
+    if (!param.key || !io || !io->rdfn || !io->wrfn) return -1;
+
+    switch (mode) {
+        case RTEA_MODE_ECB: return rteai_en_ecb(io, param, enfn);
+        case RTEA_MODE_CBC: return rteai_en_cbc(io, param, enfn);
+        case RTEA_MODE_CFB: return rteai_en_cfb(io, param, enfn);
+        case RTEA_MODE_OFB: return rteai_en_ofb(io, param, enfn);
+        case RTEA_MODE_CTR: return rteai_en_ctr(io, param, enfn);
+        case RTEA_MODE_PCBC: return rteai_en_pcbc(io, param, enfn);
+    }
+
+    return -1; /* unknown mode */
+}
+
+static int rteai_de_ecb(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out; size_t got;
+    for (;;) {
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+        if (got < RTEAI_BLKSZ) return -1;
+
+        onepass(out, in, param.key);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    }
+}
+
+static int rteai_de_cbc(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out, prev; size_t got;
+    memcpy(prev, param.init_vec, RTEAI_BLKSZ);
+    for (;;) {
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+        if (got < RTEAI_BLKSZ) return -1;
+
+        onepass(out, in, param.key);
+        rteai_xora(out, prev);
+        memcpy(prev, in, RTEAI_BLKSZ);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    }
+}
+
+static int rteai_de_cfb(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out, prev; size_t got;
+    memcpy(prev, param.init_vec, RTEAI_BLKSZ);
+    for (;;) {
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+        if (got < RTEAI_BLKSZ) return -1;
+
+        onepass(out, prev, param.key);
+        rteai_xora(out, in);
+        memcpy(prev, in, RTEAI_BLKSZ);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    }
+}
+
+static int rteai_de_ofb(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out, prev; size_t got;
+    memcpy(prev, param.init_vec, RTEAI_BLKSZ);
+    for (;;) {
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+        if (got < RTEAI_BLKSZ) return -1;
+
+        onepass(out, prev, param.key);
+        memcpy(prev, out, RTEAI_BLKSZ);
+        rteai_xora(out, in);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    }
+}
+
+static int rteai_de_ctr(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out, ctr; size_t got;
+    memcpy(ctr, param.init_vec, RTEAI_BLKSZ);
+    for (;;) {
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+        if (got < RTEAI_BLKSZ) return -1;
+
+        onepass(out, ctr, param.key);
+        rteai_xora(out, in);
+        rteai_inc(ctr);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    }
+}
+
+static int rteai_de_pcbc(
+    const rtea_io_t* io, rtea_param_t param,
+    rteai_onepass_t onepass
+) {
+    rtea_block_t in, out, prev; size_t got;
+    memcpy(prev, param.init_vec, RTEAI_BLKSZ);
+    for (;;) {
+        got = io->rdfn(in, 1, RTEAI_BLKSZ, io->rdctx);
+        if (got == 0) return 0;
+        if (got < RTEAI_BLKSZ) return -1;
+
+        onepass(out, in, param.key);
+        rteai_xora(out, prev);
+        rteai_xor(prev, in, out);
+
+        if (!io->wrfn(out, RTEAI_BLKSZ, 1, io->wrctx)) return -1;
+    }
+}
+
+static int rteai_de(
+    const rtea_io_t* io, rtea_param_t param,
+    rtea_mode_t mode, rteai_onepass_t enfn, rteai_onepass_t defn
+) {
+    if (!param.key || !io || !io->rdfn || !io->wrfn) return -1;
+
+    switch (mode) {
+        case RTEA_MODE_ECB: return rteai_de_ecb(io, param, defn);
+        case RTEA_MODE_CBC: return rteai_de_cbc(io, param, defn);
+        case RTEA_MODE_CFB: return rteai_de_cfb(io, param, enfn);
+        case RTEA_MODE_OFB: return rteai_de_ofb(io, param, enfn);
+        case RTEA_MODE_CTR: return rteai_de_ctr(io, param, enfn);
+        case RTEA_MODE_PCBC: return rteai_de_pcbc(io, param, defn);
+    }
+
+    return -1; /* unknown mode */
+}
+
+void rtea_128_onepass_en(
+          rtea_block_t out,
+    const rtea_block_t in,
+    const rtea_word_t key[4]
+) {
+    rtea_word_t L, R; int i = 0;
+    memcpy(&L, in    , 4);
+    memcpy(&R, in + 1, 4);
+    while (i < 48) {
+        R += L + ((L << 6) ^ (L >> 8)) + key[i & 3] + i; i++;
+        L += R + ((R << 6) ^ (R >> 8)) + key[i & 3] + i; i++;
+    }
+    memcpy(out    , &L, 4);
+    memcpy(out + 1, &R, 4);
+}
+
+void rtea_128_onepass_de(
+          rtea_block_t out,
+    const rtea_block_t in,
+    const rtea_word_t key[4]
+) {
+    rtea_word_t L, R; int i = 47;
+    memcpy(&L, in    , 4);
+    memcpy(&R, in + 1, 4);
+    while (i >= 0) {
+        L -= R + ((R << 6) ^ (R >> 8)) + key[i & 3] + i; i--;
+        R -= L + ((L << 6) ^ (L >> 8)) + key[i & 3] + i; i--;
+    }
+    memcpy(out    , &L, 4);
+    memcpy(out + 1, &R, 4);
+}
+
+void rtea_256_onepass_en(
+          rtea_block_t out,
+    const rtea_block_t in,
+    const rtea_word_t key[8]
+) {
+    rtea_word_t L, R; int i = 0;
+    memcpy(&L, in    , 4);
+    memcpy(&R, in + 1, 4);
+    while (i < 64) {
+        R += L + ((L << 6) ^ (L >> 8)) + key[i & 7] + i; i++;
+        L += R + ((R << 6) ^ (R >> 8)) + key[i & 7] + i; i++;
+    }
+    memcpy(out    , &L, 4);
+    memcpy(out + 1, &R, 4);
+}
+
+void rtea_256_onepass_de(
+          rtea_block_t out,
+    const rtea_block_t in,
+    const rtea_word_t key[8]
+) {
+    rtea_word_t L, R; int i = 63;
+    memcpy(&L, in    , 4);
+    memcpy(&R, in + 1, 4);
+    while (i >= 0) {
+        L -= R + ((R << 6) ^ (R >> 8)) + key[i & 7] + i; i--;
+        R -= L + ((L << 6) ^ (L >> 8)) + key[i & 7] + i; i--;
+    }
+    memcpy(out    , &L, 4);
+    memcpy(out + 1, &R, 4);
+}
+
+int rtea_128_en(const rtea_io_t* io, rtea_param_t param, rtea_mode_t mode)
+    { return rteai_en(io, param, mode, rtea_128_onepass_en, rtea_128_onepass_de); }
+int rtea_128_de(const rtea_io_t* io, rtea_param_t param, rtea_mode_t mode)
+    { return rteai_de(io, param, mode, rtea_128_onepass_en, rtea_128_onepass_de); }
+int rtea_256_en(const rtea_io_t* io, rtea_param_t param, rtea_mode_t mode)
+    { return rteai_en(io, param, mode, rtea_256_onepass_en, rtea_256_onepass_de); }
+int rtea_256_de(const rtea_io_t* io, rtea_param_t param, rtea_mode_t mode)
+    { return rteai_de(io, param, mode, rtea_256_onepass_en, rtea_256_onepass_de); }
---
+++ crypto/rtea/rtea.h
@@ -0,0 +1,58 @@
+/* Encryption and decryption functions API
+ *
+ * rtea_io_t.rdfn like a fread
+ * rtea_io_t.wrfn like a fwrite
+ *
+ * rtea_(128/256)_en returns padding byte count on success, negative value on failure
+ * rtea_(128/256)_de returns 0 on success, negative value on failure
+ */
+
+#ifndef RTEA_BLOCK_CIPHER_H
+#define RTEA_BLOCK_CIPHER_H
+
+#include <limits.h>
+#include <stddef.h>
+
+#if UINT_MAX == 0xFFFFFFFFU
+typedef unsigned rtea_word_t;
+#elif ULONG_MAX == 0xFFFFFFFFUL
+typedef unsigned long rtea_word_t;
+#else
+#  error Not detect 32-bit integer
+#endif
+
+typedef rtea_word_t rtea_block_t[2];
+
+void rtea_128_onepass_en(rtea_block_t out, const rtea_block_t in, const rtea_word_t key[4]);
+void rtea_128_onepass_de(rtea_block_t out, const rtea_block_t in, const rtea_word_t key[4]);
+
+void rtea_256_onepass_en(rtea_block_t out, const rtea_block_t in, const rtea_word_t key[8]);
+void rtea_256_onepass_de(rtea_block_t out, const rtea_block_t in, const rtea_word_t key[8]);
+
+typedef enum {
+    RTEA_MODE_ECB,
+    RTEA_MODE_CBC,
+    RTEA_MODE_CFB,
+    RTEA_MODE_OFB,
+    RTEA_MODE_CTR,
+    RTEA_MODE_PCBC
+} rtea_mode_t;
+
+typedef struct {
+    size_t (*rdfn)(      void* dst, size_t size, size_t count, void* ctx);
+    size_t (*wrfn)(const void* src, size_t size, size_t count, void* ctx);
+    void *rdctx, *wrctx;
+} rtea_io_t;
+
+typedef struct {
+    const void* key;
+    const rtea_block_t init_vec;
+} rtea_param_t;
+
+int rtea_128_en(const rtea_io_t* io, rtea_param_t param, rtea_mode_t mode);
+int rtea_128_de(const rtea_io_t* io, rtea_param_t param, rtea_mode_t mode);
+
+int rtea_256_en(const rtea_io_t* io, rtea_param_t param, rtea_mode_t mode);
+int rtea_256_de(const rtea_io_t* io, rtea_param_t param, rtea_mode_t mode);
+
+#endif /* RTEA_BLOCK_CIPHER_H */
